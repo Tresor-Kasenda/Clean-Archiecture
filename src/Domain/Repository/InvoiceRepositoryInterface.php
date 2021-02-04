9bc058e5521580bc1839cf64866806b2a57e96d6
tresor <scotttresor@gmail.com>
2021-02-04 16:37:00 +0200

    [feat]: add InvoiceRepositoryInterface

---
+++ src/Domain/Repository/InvoiceRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace CleanPHP\Invoicer\Domain\Repository;
+
+/**
+ * Interface InvoiceRepositoryInterface
+ * @package CleanPHP\Invoicer\Domain\Repository
+ */
+interface InvoiceRepositoryInterface extends RepositoryInterface
+{
+
+}
