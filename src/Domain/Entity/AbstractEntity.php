ddce84957aed9b30c5e9784dd61a3e2a1ffe68f7
tresor <scotttresor@gmail.com>
2021-02-04 15:44:45 +0200

    [feat]: add abstractEntity

---
+++ src/Domain/Entity/AbstractEntity.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace CleanPHP\Invoicer\Domain\Entity;
+
+/**
+ * Class AbstractEntity
+ * @package CleanPHP\Invoicer\Domain\Entity
+ */
+abstract class AbstractEntity
+{
+    protected int $id;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setId(int $id): AbstractEntity
+    {
+        $this->id = $id;
+        return $this;
+    }
+}
