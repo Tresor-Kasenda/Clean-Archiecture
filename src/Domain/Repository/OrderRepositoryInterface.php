16803a3c4bf4efc968e6123bc6c9eeb1271fef1d
tresor <scotttresor@gmail.com>
2021-02-04 16:37:09 +0200

    [feat]: add OrderRepositoryInterface

---
+++ src/Domain/Repository/OrderRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace CleanPHP\Invoicer\Domain\Repository;
+
+/**
+ * Class OrderRepositoryInterface
+ * @package CleanPHP\Invoicer\Domain\Repository
+ */
+interface OrderRepositoryInterface extends RepositoryInterface
+{
+
+}
